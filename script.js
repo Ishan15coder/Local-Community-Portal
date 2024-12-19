fb567f288b2c239e41726b919119763deed11fb0
ishan gupta <gishan750@gmail.com>
2024-12-19 16:42:12 +0530

    thirdcomm

--- script.js
+++
@@ -1,5 +0,0 @@
-// Example: Alert when form is submitted
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you! Your query has been submitted.');
-});

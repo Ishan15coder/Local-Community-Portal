e334cd881b0cb7b7aaede63c5733845ab6ff9226
ishan gupta <gishan750@gmail.com>
2024-12-19 16:23:22 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,5 @@
+// Example: Alert when form is submitted
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you! Your query has been submitted.');
+});
